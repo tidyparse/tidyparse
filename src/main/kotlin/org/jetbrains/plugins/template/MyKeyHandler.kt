39c3b0c7ca9e2185c58e8414c8ee17714810eed4
breandan <bre@ndan.co>
2022-05-11 07:59:00 -0400

    render status and add scroll pane

--- src/main/kotlin/org/jetbrains/plugins/template/MyKeyHandler.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/MyKeyHandler.kt
@@ -3,23 +3,22 @@ package org.jetbrains.plugins.template
 import ai.hypergraph.kaliningraph.parsing.CFG
 import ai.hypergraph.kaliningraph.parsing.parse
 import ai.hypergraph.kaliningraph.parsing.parseCFG
-import ai.hypergraph.kaliningraph.parsing.solveFixedpoint
 import ai.hypergraph.kaliningraph.sat.synthesizeFromFPSolving
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate.Result.CONTINUE
-import com.intellij.notification.NotificationGroupManager
-import com.intellij.notification.NotificationType
 import com.intellij.openapi.application.ReadAction
-import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
 import org.jetbrains.concurrency.runAsync
 
 class MyKeyHandler : TypedHandlerDelegate() {
     lateinit var cfg: CFG
     var grammarLastModified = 0L
+    val ok = "✅ Current line parses\n"
+    val no = "❌ Current line invalid\n"
+
     override fun charTyped(c: Char, project: Project, editor: Editor, file: PsiFile): Result {
         if (!file.name.endsWith(".tidy")) return CONTINUE
         val grammarFile = file.containingDirectory.files
@@ -34,10 +33,13 @@ class MyKeyHandler : TypedHandlerDelegate() {
             if (grammarFile.modificationStamp != grammarLastModified)
                 cfg = ReadAction.compute<String, Exception> { grammarFile.text }.parseCFG()
 
-            if ("_" !in editor.document.text)
-                cfg.parse(editor.document.text)?.let { MyToolWindow.textField.text = it.prettyPrint() }
-            else editor.document.text.synthesizeFromFPSolving(cfg).take(40).toList().shuffled().let {
-                if (it.isNotEmpty()) MyToolWindow.textField.text = it.joinToString("\n")
+            val (lineStart, lineEnd) = editor.caretModel.let { it.visualLineStart to it.visualLineEnd }
+            val currentLine = editor.document.getText(TextRange.create(lineStart, lineEnd))
+
+            if ("_" !in currentLine)
+                cfg.parse(currentLine)?.let { MyToolWindow.textArea.text = ok + it.prettyPrint() }
+            else currentLine.synthesizeFromFPSolving(cfg).take(20).toList().shuffled().let {
+                if (it.isNotEmpty()) MyToolWindow.textArea.text = it.joinToString("\n")
             }
         }
 
--- src/main/kotlin/org/jetbrains/plugins/template/MyToolWindow.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/MyToolWindow.kt
@@ -3,21 +3,25 @@ package org.jetbrains.plugins.template
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.content.ContentFactory.SERVICE
 import java.awt.Font
+import javax.swing.JScrollPane
 import javax.swing.JTextArea
-import javax.swing.JTextField
 
 class MyToolWindowFactory: ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val contentFactory = SERVICE.getInstance()
-        val content = contentFactory.createContent(MyToolWindow.textField, "", false)
+        val content = contentFactory.createContent(MyToolWindow.panel, "", false)
         toolWindow.contentManager.addContent(content)
     }
 }
 
 object MyToolWindow {
-    val textField = JTextArea().apply {
+    val textArea = JTextArea().apply {
         font = Font("JetBrains Mono", Font.PLAIN, 16)
+        isEditable = false
     }
+
+    val panel = JBScrollPane(textArea)
 }
