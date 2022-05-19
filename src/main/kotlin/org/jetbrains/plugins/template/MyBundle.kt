a6a90a1b9434378a7181537e5ee33283c6dd15ac
breandan <bre@ndan.co>
2022-05-18 20:35:59 -0400

    update CI

--- src/main/kotlin/org/jetbrains/plugins/template/MyBundle.kt
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.plugins.template
-
-import com.intellij.DynamicBundle
-import org.jetbrains.annotations.NonNls
-import org.jetbrains.annotations.PropertyKey
-
-@NonNls
-private const val BUNDLE = "messages.MyBundle"
-
