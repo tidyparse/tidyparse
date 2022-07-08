8f030912ca288fad534235c8091fafbea0504ab7
breandan <bre@ndan.co>
2022-07-07 19:57:35 -0700

    try validating grammar itself

--- src/main/kotlin/ai/hypergraph/tidyparse/TidyKeyHandler.kt
+++ src/main/kotlin/ai/hypergraph/tidyparse/TidyKeyHandler.kt
@@ -1,9 +1,6 @@
 package ai.hypergraph.tidyparse
 
-import ai.hypergraph.kaliningraph.parsing.CFG
-import ai.hypergraph.kaliningraph.parsing.parse
-import ai.hypergraph.kaliningraph.parsing.parseCFG
-import ai.hypergraph.kaliningraph.parsing.prettyPrint
+import ai.hypergraph.kaliningraph.parsing.*
 import ai.hypergraph.kaliningraph.sat.synthesizeFrom
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate.Result.CONTINUE
@@ -11,13 +8,14 @@ import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.updateSettings.impl.Product
 import com.intellij.psi.PsiFile
 import org.jetbrains.concurrency.runAsync
 
 var grammarFileCache: String? = ""
 lateinit var cfg: CFG
 
-fun PsiFile.recomputeGrammar(): Set<Pair<String, List<String>>> =
+fun PsiFile.recomputeGrammar(): CFG =
   runReadAction {
     val grammar = text.substringBefore("---")
     if (grammar != grammarFileCache) {
@@ -32,13 +30,20 @@ class TidyKeyHandler : TypedHandlerDelegate() {
 
   override fun charTyped(c: Char, project: Project, editor: Editor, file: PsiFile) =
     CONTINUE.also {
-      if (file.name.endsWith(".tidy") && editor.caretModel.offset > editor.document.text.lastIndexOf("---"))
-        file.reconcile(editor.currentLine())
+      if (file.name.endsWith(".tidy"))
+        file.reconcile(
+          currentLine = editor.currentLine(),
+          isInGrammar = editor.caretModel.offset < editor.document.text.lastIndexOf("---")
+        )
     }
 
-  private fun PsiFile.reconcile(currentLine: String) =
+  private fun PsiFile.reconcile(currentLine: String, isInGrammar: Boolean) =
     runAsync {
-      cfg = recomputeGrammar()
+      cfg =
+        if (isInGrammar)
+          CFGCFG(names = currentLine.split(Regex("\\s+"))
+            .filter { it.isNotBlank() && it.contains("\\w") })
+        else recomputeGrammar()
 
       var debugText = ""
       if ("_" !in currentLine && Regex("<[^\\s>]*>") !in currentLine) {
