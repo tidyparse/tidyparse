1f618760d17734e559939b317f5d897b4a31568f
breandan <bre@ndan.co>
2022-07-25 11:56:31 -0400

    fix debug text

--- src/main/kotlin/ai/hypergraph/tidyparse/TidyFileType.kt
+++ src/main/kotlin/ai/hypergraph/tidyparse/TidyFileType.kt
@@ -2,6 +2,7 @@ package ai.hypergraph.tidyparse
 
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.util.PlatformIcons
+import javax.swing.Icon
 
 object TidyFileType: FileType {
   override fun getName() = "Tidy File"
@@ -10,7 +11,7 @@ object TidyFileType: FileType {
 
   override fun getDefaultExtension() = "tidy"
 
-  override fun getIcon() = PlatformIcons.EDIT
+  override fun getIcon(): Icon = PlatformIcons.EDIT
 
   override fun isBinary() = false
 }
--- src/main/kotlin/ai/hypergraph/tidyparse/TidyKeyHandler.kt
+++ src/main/kotlin/ai/hypergraph/tidyparse/TidyKeyHandler.kt
@@ -1,8 +1,10 @@
 package ai.hypergraph.tidyparse
 
+import ai.grazie.nlp.utils.takeNonWhitespaces
 import ai.hypergraph.kaliningraph.parsing.*
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate.Result.CONTINUE
+import com.intellij.grazie.utils.dropPrefix
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.editor.Editor
@@ -19,7 +21,8 @@ fun PsiFile.recomputeGrammar(): CFG =
     val grammar = text.substringBefore("---")
     if (grammar != grammarFileCache) {
       grammarFileCache = grammar
-      ReadAction.compute<String, Exception> { grammarFileCache }.parseCFG()
+      ReadAction.compute<String, Exception> { grammarFileCache }
+        .parseCFG().also { cfg = it }
     } else cfg
   }
 
@@ -29,35 +32,48 @@ class TidyKeyHandler : TypedHandlerDelegate() {
 
   override fun charTyped(c: Char, project: Project, editor: Editor, file: PsiFile) =
     CONTINUE.also {
-      if (file.name.endsWith(".tidy"))
+      if (file.name.endsWith(".tidy")) {
         file.reconcile(
           currentLine = editor.currentLine(),
           isInGrammar = editor.caretModel.offset < editor.document.text.lastIndexOf("---")
         )
 
-      ToolWindowManager.getInstance(project).getToolWindow("Tidyparse")
-        ?.let { if (!it.isVisible) it.show() }
+        ToolWindowManager.getInstance(project).getToolWindow("Tidyparse")
+          ?.let { if (!it.isVisible) it.show() }
+      }
     }
 
   private fun PsiFile.reconcile(currentLine: String, isInGrammar: Boolean) =
     runAsync {
-      cfg =
+      val cfg =
         if (isInGrammar)
           CFGCFG(names = currentLine.split(Regex("\\s+"))
-            .filter { it.isNotBlank() && it.contains("\\w") })
+            .filter { it.isNotBlank() && it !in setOf("->", "|") }.toSet()
+          )
         else recomputeGrammar()
 
       var debugText = ""
-      if ("_" !in currentLine && Regex("<[^\\s>]*>") !in currentLine) {
+      if (currentLine.containsHole()) {
+        synchronized(cfg) {
+          synth(currentLine, cfg).let {
+            if (it.isNotEmpty()) debugText = it.joinToString("\n")
+          }
+        }
+      } else {
         val parse = cfg.parse(currentLine)
         debugText = if (parse != null) ok + parse.prettyPrint()
-        else no + TidyToolWindow.textArea.text.drop(ok.length)
-      } else synchronized(cfg) {
-        synth(currentLine, cfg).let { if (it.isNotEmpty()) debugText = it.joinToString("\n") }
+        else no + TidyToolWindow.textArea.text.dropPrefix(ok).dropPrefix(no)
       }
 
-      val delim = List(50) { "─" }.joinToString("", "\n", "\n")
-      debugText += delim + "Chomsky normal form:\n${cfg.prettyPrint(3)}"
+      // Append the CFG only if parse succeeds
+      if (!debugText.startsWith(no)) {
+        val delim = List(50) { "─" }.joinToString("", "\n", "\n")
+        debugText += delim + "Chomsky normal form:\n${cfg.prettyPrint(3)}"
+      }
+
       TidyToolWindow.textArea.text = debugText
     }
+
+  private fun String.containsHole(): Boolean =
+    "_" in this || Regex("<[^\\s>]*>") in this
 }
