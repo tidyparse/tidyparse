51f7d13b9d2e44a8e9fa0367a020eaf5e9c983a4
breandan <bre@ndan.co>
2022-08-19 17:50:22 -0400

    remove deprecated API

--- src/main/kotlin/ai/hypergraph/tidyparse/TidyToolWindow.kt
+++ src/main/kotlin/ai/hypergraph/tidyparse/TidyToolWindow.kt
@@ -4,15 +4,14 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBScrollPane
-import com.intellij.ui.content.ContentFactory.SERVICE
+import com.intellij.ui.content.ContentFactory
 import java.awt.Font
-import javax.swing.JTextArea
 import javax.swing.JTextPane
 
 class TidyToolWindowFactory : ToolWindowFactory {
   override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) =
     toolWindow.contentManager.addContent(
-      SERVICE.getInstance().createContent(TidyToolWindow.panel, "", false)
+      ContentFactory.getInstance().createContent(TidyToolWindow.panel, "", false)
     )
 }
 
