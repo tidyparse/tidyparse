5f652be31660894d593bab9e514f636ec60c1939
breandan <bre@ndan.co>
2022-08-21 12:55:24 -0400

    thread safety improvements

--- src/main/kotlin/ai/hypergraph/tidyparse/TidyToolWindow.kt
+++ src/main/kotlin/ai/hypergraph/tidyparse/TidyToolWindow.kt
@@ -1,5 +1,6 @@
 package ai.hypergraph.tidyparse
 
+import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
@@ -7,6 +8,7 @@ import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.content.ContentFactory
 import java.awt.Font
 import javax.swing.JTextPane
+import javax.swing.text.DefaultCaret
 
 class TidyToolWindowFactory : ToolWindowFactory {
   override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) =
@@ -16,18 +18,22 @@ class TidyToolWindowFactory : ToolWindowFactory {
 }
 
 object TidyToolWindow {
-  val textArea = JTextPane().apply {
+  private val textArea = JTextPane().apply {
     contentType = "text/html"
     font = Font("JetBrains Mono", Font.PLAIN, 16)
     isEditable = false
+    (caret as DefaultCaret).updatePolicy = DefaultCaret.NEVER_UPDATE
   }
 
+  @Volatile
   var lastUpdate = ""
   var text: String
-    get() = textArea.text
+    get() = lastUpdate
     set(s) {
-      if (s.length != lastUpdate.length || s != lastUpdate)
-        textArea.text = s.also { lastUpdate = it }
+      if (s.length != lastUpdate.length || s != lastUpdate) {
+        lastUpdate = s
+        invokeLater { textArea.text = s }
+      }
     }
 
   val panel = JBScrollPane(textArea)
