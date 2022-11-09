f3e55b54794f7d77ae60ef744c0e26804f4c7efa
breandan <bre@ndan.co>
2022-11-09 10:20:14 -0500

    update deps, drop jitpack repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
   // Kotlin support
-  kotlin("jvm") version "1.7.20"
+  kotlin("jvm") version "1.7.21"
   // Gradle IntelliJ Plugin
   id("org.jetbrains.intellij") version "1.9.0"
   // Gradle Changelog Plugin
