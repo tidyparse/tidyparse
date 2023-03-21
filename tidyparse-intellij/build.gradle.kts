6efc8f0ed35dd1791592f65524a3e47af1934edb
breandan <bre@ndan.co>
2023-03-21 13:58:08 -0400

    switch to new property assignment syntax

--- tidyparse-intellij/build.gradle.kts
+++ tidyparse-intellij/build.gradle.kts
@@ -16,18 +16,18 @@ version = properties("pluginVersion")
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
-  pluginName.set(properties("pluginName"))
-  version.set(properties("platformVersion"))
-  type.set(properties("platformType"))
+  pluginName = properties("pluginName")
+  version = properties("platformVersion")
+  type = properties("platformType")
 
   // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-  plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+  plugins = properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty)
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
 changelog {
-  version.set(properties("pluginVersion"))
-  groups.set(emptyList())
+  version = properties("pluginVersion")
+  groups = emptyList()
 }
 
 dependencies {
@@ -80,12 +80,12 @@ tasks {
   }
 
   patchPluginXml {
-    version.set(properties("pluginVersion"))
-    sinceBuild.set(properties("pluginSinceBuild"))
-    untilBuild.set(properties("pluginUntilBuild"))
+    version = properties("pluginVersion")
+    sinceBuild = properties("pluginSinceBuild")
+    untilBuild = properties("pluginUntilBuild")
 
 // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-    pluginDescription.set(
+    pluginDescription = 
       projectDir.parentFile.resolve("README.md").readText().lines().run {
         val start = "<!-- Plugin description -->"
         val end = "<!-- Plugin description end -->"
@@ -94,16 +94,15 @@ tasks {
           throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
         subList(indexOf(start) + 1, indexOf(end))
       }.joinToString("\n").run { markdownToHTML(this) }
-    )
 
 // Get the latest available change notes from the changelog file
-    changeNotes.set(provider {
+    changeNotes = provider {
       changelog.renderItem(changelog.getAll().values.first(), HTML)
-    })
+    }
   }
 
   runPluginVerifier {
-    ideVersions.set(listOf("2022.3"))
+    ideVersions = listOf("2022.3")
   }
 
   runIde {
@@ -121,17 +120,17 @@ tasks {
 //  }
 //
 //  signPlugin {
-//    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-//    privateKey.set(System.getenv("PRIVATE_KEY"))
-//    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+//    certificateChain = System.getenv("CERTIFICATE_CHAIN"))
+//    privateKey = System.getenv("PRIVATE_KEY"))
+//    password = System.getenv("PRIVATE_KEY_PASSWORD"))
 //  }
 //
 //  publishPlugin {
 //    dependsOn("patchChangelog")
-//    token.set(System.getenv("PUBLISH_TOKEN"))
+//    token = System.getenv("PUBLISH_TOKEN"))
 //    // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
 //    // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
 //    // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-//    channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
+//    channels = listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
 //  }
 }
