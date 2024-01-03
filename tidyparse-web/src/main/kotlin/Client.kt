8fe938363a811299a430089f1f98c69f10b5635d
breandan <bre@ndan.co>
2024-01-03 13:40:02 -0500

    increase timeout

--- tidyparse-web/src/main/kotlin/Client.kt
+++ tidyparse-web/src/main/kotlin/Client.kt
@@ -46,7 +46,7 @@ fun main() {
   if (window.navigator.userAgent.indexOf("hrome") != -1) {
     PlatformVars.PLATFORM_CALLER_STACKTRACE_DEPTH = 4
   }
-  TIMEOUT_MS = 5_000
+  TIMEOUT_MS = 10_000
   jsEditor.getLatestCFG()
   window.onload = { jsEditor.redecorateLines() }
   inputField.addEventListener("input", { jsEditor.run { continuation { handleInput() } } })
