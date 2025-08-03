2400d34a5122e0a3ad10f10ca7548473fc097ecb
breandan <bre@ndan.co>
2025-08-02 21:21:56 -0400

    custom launchers

--- tidyparse-web/karma.config.d/karma.conf.js
+++ tidyparse-web/karma.config.d/karma.conf.js
@@ -10,4 +10,26 @@ config.set({
     //     format: '%b %T: %m',  // Browser, time, message
     //     terminal: true  // Output to terminal
     // },
+    customLaunchers: {
+        ChromeSmall: {
+            base: 'Chrome',
+            flags: ['--window-size=1,1']
+        }
+    },
+    browsers: ['ChromeSmall'],
+    // customLaunchers: {
+    //     ChromeHeadlessWebGPU: {
+    //         base: 'ChromeHeadless',
+    //         flags: [
+    //             '--enable-unsafe-webgpu',
+    //             // '--use-angle=swiftshader',  // CPU fallback
+    //             '--disable-gpu-driver-bug-workarounds',
+    //             '--enable-logging=stderr',  // Route Chrome logs to stderr (visible in Gradle)
+    //             '--v=1',  // Verbose Chrome logging
+    //             '--no-sandbox',  // Required for some CI/headless envs
+    //             '--headless=new'  // New headless mode (better stability in Chrome 109+)
+    //         ]
+    //     }
+    // },
+    // browsers: ['ChromeHeadlessWebGPU']
 });
